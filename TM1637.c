97ded9cb26d2c95035c623742c8b6e51ef2f72a4
Albin Wahlén <alegow8@gmail.com>
2021-04-02 14:43:41 +0200

    Display 4 digit number

--- TM1637.c
+++ TM1637.c
@@ -1,4 +1,6 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 // Our assembled program:
@@ -8,10 +10,12 @@
 #define DIO_PIN 27
 #define SET_WRITEMODE 0x40
 #define WRITE_ADDRESS 0xc0
+#define BRIGHTNESS_BASE 0x88
+#define MAX_DIGITS 4
 
 /* Global variables */
 PIO pio;
-uint clkPin, dioPin, sm;
+uint clkPin, dioPin, sm, brightness = 0;
 
 const uint8_t digitToSegment[] = {
   0b00111111,    // 0
@@ -59,6 +63,10 @@ void TM1637_init(uint clk, uint dio) {
   stdio_init_all();
 }
 
+void TM1637_on() {
+  pio_sm_put_blocking(pio, sm, BRIGHTNESS_BASE + brightness);
+}
+
 /* Put one or two bytes on TM1637 display. 
  *
  * start_pos: uint from 0 to 3 where 0 is to the left
@@ -69,6 +77,11 @@ void TM1637_put_2_bytes(uint start_pos, uint data) {
   pio_sm_put_blocking(pio, sm, (data << 16) + (address << 8) +  SET_WRITEMODE);
 }
 
+/* Put one to four bytes on TM1637 display. 
+ *
+ * start_pos: uint from 0 to 3 where 0 is to the left
+ * data: raw data for one to four bytes, the first byte will be but to the left
+ */
 void TM1637_put_4_bytes(uint start_pos, uint data) {
   uint address = WRITE_ADDRESS + start_pos;
   uint data1 = data & 0xffff;  // first two bytes
@@ -76,7 +89,72 @@ void TM1637_put_4_bytes(uint start_pos, uint data) {
   pio_sm_put_blocking(pio, sm, (data1 << 16) + (address << 8) + SET_WRITEMODE);
   pio_sm_put_blocking(pio, sm, data2 << 16);  // I have no idea why this has to be shifted
 }
+
+/* Convert a number to something readable for the 'put bytes' functions.
+ *
+ * Warning, input must not be more than 4 digits.
+ */
+unsigned int num_to_hex(int num) {
+  unsigned int hex = 0x0, seg;
+  while (num) {
+    seg = digitToSegment[num % 10];  // extract last digit as 7 segment byte
+    num /= 10;  // remove last digit from num
+    hex = seg + (hex << 8);  // Put new segment to the right in hex
+  }
+  return hex;
+}
+
+/* Display a number with 4 digits.
+ *
+ * The least significat digit will be put to the right.
+ * Arguments:
+ *  int number: The number to display (currently only positve)
+ *  bool leadingZeros: If leading Zeros should be displayed or not.
+ */
+void TM1637_display(int number, bool leadingZeros) { 
+  // Determine length of number
+  int len = 0;
+  int numberCopy = number;
+  while (numberCopy) {
+    len++;
+    numberCopy /= 10;
+  }
+  printf("len = %d, num = %d\n", len, number);
+  if (len > 4) {
+    printf("Warning number %d too long\n", number);
+    len = 4;
+    // least signigicant digits will be cut of in num_to_hex
+  }
+
+  // Get hex
+  unsigned int hex = num_to_hex(number);
+  unsigned int startPos = 0;
+  if (leadingZeros && (len < MAX_DIGITS)) {
+    printf("Leading zeros and short\n");
+    for (int i=len; i<MAX_DIGITS; i++) {
+      hex = (hex << 8) + digitToSegment[0];
+    }
+  } else if (number == 0) {
+    // Signular case
+    hex = digitToSegment[0];
+    startPos = MAX_DIGITS - 1;
+  } else {
+    printf("Normal case\n");
+    startPos = MAX_DIGITS - len;
+  }
   
+  // Display number
+  TM1637_put_4_bytes(startPos, hex);
+  TM1637_on();
+}
+    
+  
+void TM1637_clear() {
+  pio_sm_put_blocking(pio, sm, 0x80);
+  pio_sm_put_blocking(pio, sm, 0xc040);
+  pio_sm_put_blocking(pio, sm, 0x0);
+}
+
 
 
 int main()
@@ -85,37 +163,25 @@ int main()
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    TM1637_init(CLK_PIN, DIO_PIN);
-
-
-    // Light up all segments:
-    //pio_sm_put_blocking(pio, 0, 0xffc0448a);
-    //pio_sm_put_blocking(pio, 0, 0xffc1448a);
-    //pio_sm_put_blocking(pio, 0, 0xffc2448a);
-    //pio_sm_put_blocking(pio, 0, 0xffc3448a);
-
-    //sleep_ms(3000);
-
-    // Wait 3 seconds, then print 123 with max brightness:    
-    pio_sm_put_blocking(pio, sm, 0x80);  // Clear display
-    pio_sm_put_blocking(pio, sm, 0xc040);
-    pio_sm_put_blocking(pio, sm, 0x0);
-
+    TM1637_init(CLK_PIN, DIO_PIN);  
+    TM1637_display(8888, true); 
     sleep_ms(500);
-    //pio_sm_put(pio, sm, 0x3f3fc140);
-    //pio_sm_put(pio, sm, 0x773f0000);
     
-    TM1637_put_4_bytes(0, 0x4f4fbf06);
-    //sleep_ms(100);
-    pio_sm_put_blocking(pio, sm, 0x8a);
+    TM1637_clear();
+    sleep_ms(500);
+
+    int count = 0;
 
     // just wait here and blink LED like usual...
     while(1) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(500);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(500);
+      TM1637_display(count, false);
+      count++;
+
+      gpio_put(LED_PIN, 1);
+      sleep_ms(500);
+      gpio_put(LED_PIN, 0);
+      sleep_ms(500);
     }
 
     return 0;
-}
+} 

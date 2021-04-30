bd8041ef242de5aa675e96fe87cbe4d7e9050ae0
Albin Wahlén <alegow8@gmail.com>
2021-04-30 20:34:23 +0200

    Add the docs html

---
+++ docs/html/search/all_1.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['tm1637_5fclear',['TM1637_clear',['../PicoTM1637_8h.html#aefba0c30895ce3724b1088ee6884b0a7',1,'PicoTM1637.h']]],
+  ['tm1637_5fdisplay',['TM1637_display',['../PicoTM1637_8h.html#afadf896c147664d381078a71aa67ffe6',1,'PicoTM1637.h']]],
+  ['tm1637_5fdisplay_5fboth',['TM1637_display_both',['../PicoTM1637_8h.html#ad8616b2f269afd07083989f6510f2602',1,'PicoTM1637.h']]],
+  ['tm1637_5fdisplay_5fleft',['TM1637_display_left',['../PicoTM1637_8h.html#aa172e94f7a4994113ace2f161662f737',1,'PicoTM1637.h']]],
+  ['tm1637_5fdisplay_5fright',['TM1637_display_right',['../PicoTM1637_8h.html#acca9fd207b506432791ff075955deb23',1,'PicoTM1637.h']]],
+  ['tm1637_5fdisplay_5fword',['TM1637_display_word',['../PicoTM1637_8h.html#aa79d2b795727374b09dd278218381ecb',1,'PicoTM1637.h']]],
+  ['tm1637_5finit',['TM1637_init',['../PicoTM1637_8h.html#aa0044fd1a0fade54f5c818c1e2cbcd3f',1,'PicoTM1637.h']]],
+  ['tm1637_5fput_5f2_5fbytes',['TM1637_put_2_bytes',['../PicoTM1637_8h.html#a9e4f1829a1c6291ae04933547cd5b40d',1,'PicoTM1637.h']]],
+  ['tm1637_5fput_5f4_5fbytes',['TM1637_put_4_bytes',['../PicoTM1637_8h.html#a307c3401c00746da1b0fc1da29a19e4c',1,'PicoTM1637.h']]],
+  ['tm1637_5fset_5fbrightness',['TM1637_set_brightness',['../PicoTM1637_8h.html#ab7eef453cfb24dbf694bf168539dc5c6',1,'PicoTM1637.h']]],
+  ['tm1637_5fset_5fcolon',['TM1637_set_colon',['../PicoTM1637_8h.html#a8a25666a00988ca9f8156fcf592957ed',1,'PicoTM1637.h']]]
+];
